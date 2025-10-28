e22fef210029bf839a08e54c0a83c418c210f72a
ortonwang <94680642+ortonwang@users.noreply.github.com>
2025-10-28 16:03:57 +0800

    Update test_patch.py

--- code/utils/test_patch.py
+++ code/utils/test_patch.py
@@ -103,6 +103,106 @@ def test_all_case(model_name, num_outputs, model, image_list, num_classes, patch
             f.writelines('average metric of all decoders is {} \n'.format(avg_metric_average))
     return avg_metric
 
+def var_all_case_base_out(model, num_classes, patch_size=(112, 112, 80), stride_xy=18, stride_z=4, dataset_name="LA"):
+    if dataset_name == "LA":
+        with open('../data/LA/test.list', 'r') as f:
+            image_list = f.readlines()
+        image_list = ["../data/LA/2018LA_Seg_Training Set/" + item.replace('\n', '') + "/mri_norm2.h5" for item in image_list]
+    elif dataset_name == "Pancreas_CT":
+        with open('../data/Pancreas/test.list', 'r') as f:
+            image_list = f.readlines()
+        image_list = ["../data/Pancreas/Pancreas_h5/" + item.replace('\n', '') + "_norm.h5" for item in image_list]#[:2]
+        # image_list = ["../data/Pancreas/Pancreas_h5/" + item.replace('\n', '') + "/mri_norm2.h5" for item in image_list]
+    elif dataset_name == "BraTS2019":
+        with open('../data/BraTS2019/val.txt', 'r') as f:
+            image_list = f.readlines()
+        image_list =["../data/BraTS2019/data/"  + item.replace('\n', '') + ".h5" for item in image_list]
+    else:
+        print('false')
+        # image_list = ["./data/Pancreas/Pancreas_h5/" + item.replace('\n', '') + "_norm.h5" for item in image_list]
+    # loader = tqdm(image_list)
+    total_dice = 0.0
+    for image_path in tqdm(image_list):
+        h5f = h5py.File(image_path, 'r')
+        image = h5f['image'][:]
+        label = h5f['label'][:]
+        prediction, score_map = test_single_case_first_output(model, image, stride_xy, stride_z, patch_size, num_classes=num_classes)
+        if np.sum(prediction)==0:
+            dice = 0
+        else:
+            dice = metric.binary.dc(prediction, label)
+        total_dice += dice
+    avg_dice = total_dice / len(image_list)
+    print('average metric is {}'.format(avg_dice))
+    return avg_dice
+
+
+def test_single_case_first_output(model, image, stride_xy, stride_z, patch_size, num_classes=1):
+    w, h, d = image.shape
+
+    # if the size of image is less than patch_size, then padding it
+    add_pad = False
+    if w < patch_size[0]:
+        w_pad = patch_size[0]-w
+        add_pad = True
+    else:
+        w_pad = 0
+    if h < patch_size[1]:
+        h_pad = patch_size[1]-h
+        add_pad = True
+    else:
+        h_pad = 0
+    if d < patch_size[2]:
+        d_pad = patch_size[2]-d
+        add_pad = True
+    else:
+        d_pad = 0
+    wl_pad, wr_pad = w_pad//2,w_pad-w_pad//2
+    hl_pad, hr_pad = h_pad//2,h_pad-h_pad//2
+    dl_pad, dr_pad = d_pad//2,d_pad-d_pad//2
+    if add_pad:
+        image = np.pad(image, [(wl_pad,wr_pad),(hl_pad,hr_pad), (dl_pad, dr_pad)], mode='constant', constant_values=0)
+    ww,hh,dd = image.shape
+
+    sx = math.ceil((ww - patch_size[0]) / stride_xy) + 1
+    sy = math.ceil((hh - patch_size[1]) / stride_xy) + 1
+    sz = math.ceil((dd - patch_size[2]) / stride_z) + 1
+    # print("{}, {}, {}".format(sx, sy, sz))
+    score_map = np.zeros((num_classes, ) + image.shape).astype(np.float32)
+    cnt = np.zeros(image.shape).astype(np.float32)
+
+    for x in range(0, sx):
+        xs = min(stride_xy*x, ww-patch_size[0])
+        for y in range(0, sy):
+            ys = min(stride_xy * y,hh-patch_size[1])
+            for z in range(0, sz):
+                zs = min(stride_z * z, dd-patch_size[2])
+                test_patch = image[xs:xs+patch_size[0], ys:ys+patch_size[1], zs:zs+patch_size[2]]
+                test_patch = np.expand_dims(np.expand_dims(test_patch,axis=0),axis=0).astype(np.float32)
+                test_patch = torch.from_numpy(test_patch).cuda()
+
+                with torch.no_grad():
+                    y = model(test_patch)#[0]
+                    if len(y) > 1:
+                        y = y[0]
+                    y = F.softmax(y, dim=1)
+                y = y.cpu().data.numpy()
+                y = y[0,1,:,:,:]
+                score_map[:, xs:xs+patch_size[0], ys:ys+patch_size[1], zs:zs+patch_size[2]] \
+                  = score_map[:, xs:xs+patch_size[0], ys:ys+patch_size[1], zs:zs+patch_size[2]] + y
+                cnt[xs:xs+patch_size[0], ys:ys+patch_size[1], zs:zs+patch_size[2]] \
+                  = cnt[xs:xs+patch_size[0], ys:ys+patch_size[1], zs:zs+patch_size[2]] + 1
+
+    score_map = score_map/np.expand_dims(cnt,axis=0)
+    # ac = np.argmax(score_map, axis=0)
+    # ab = score_map[0]
+    label_map = (score_map[0]>0.5).astype(np.int32)
+    if add_pad:
+        label_map = label_map[wl_pad:wl_pad+w,hl_pad:hl_pad+h,dl_pad:dl_pad+d]
+        score_map = score_map[:,wl_pad:wl_pad+w,hl_pad:hl_pad+h,dl_pad:dl_pad+d]
+    return label_map, score_map
+
+
 def test_single_case(model, image, stride_xy, stride_z, patch_size, num_classes=1):
     w, h, d = image.shape
 
